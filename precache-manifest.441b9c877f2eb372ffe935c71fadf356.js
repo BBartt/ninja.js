433d0527f64ecc8c120e5a8cb435c9f3aaa47985
esbbartt <bartosz.polakowski@escola.pl>
2021-03-05 10:30:13 +0100

    Updates

---
+++ precache-manifest.441b9c877f2eb372ffe935c71fadf356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ff7f69c8f216acbd05915b79606466",
+    "url": "/ninja.js/index.html"
+  },
+  {
+    "revision": "17a517e62ad295144952",
+    "url": "/ninja.js/static/css/main.bc0b1c70.chunk.css"
+  },
+  {
+    "revision": "ec99bc9d79fee2ba894c",
+    "url": "/ninja.js/static/js/2.dfcb037a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ninja.js/static/js/2.dfcb037a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a517e62ad295144952",
+    "url": "/ninja.js/static/js/main.dc0c5561.chunk.js"
+  },
+  {
+    "revision": "05c947b406dde625889a",
+    "url": "/ninja.js/static/js/runtime-main.f0994713.js"
+  }
+]);
